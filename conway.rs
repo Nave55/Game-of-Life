bd04abae504d1f04fd2ff4924ee79cd98caa2054
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-01-16 02:15:54 -0700

    Update conway.rs

--- conway.rs
+++ conway.rs
@@ -12,18 +12,24 @@ static DARK_GREY: Color = Color {
     b: 55,
     a: 255,
 };
+static GREEN: Color = Color {
+    r: 0,
+    g: 228,
+    b: 48,
+    a: 255,
+};
+
+const WIDTH: i32 = 1020;
+const HEIGHT: i32 = 1020;
+const CELL_SIZE: usize = 6;
+const ROWS: usize = (HEIGHT / CELL_SIZE as i32) as usize;
+const COLS: usize = (WIDTH / CELL_SIZE as i32) as usize;
 
 fn main() {
-    const WIDTH: i32 = 1020;
-    const HEIGHT: i32 = 1020;
-    const CELL_SIZE: usize = 6;
-    const ROWS: usize = (HEIGHT / CELL_SIZE as i32) as usize;
-    const COLS: usize = (WIDTH / CELL_SIZE as i32) as usize;
-
     let mut fps: u32 = 12;
     let mut running = false;
-    let mut cells: Vec<Vec<u32>> = vec![vec![0; COLS]; ROWS];
-    let mut tmp_cells: Vec<Vec<u32>> = vec![vec![0; COLS]; ROWS];
+    let mut cells: [[u32; COLS]; ROWS] = [[0; COLS]; ROWS];
+    let mut tmp_cells: [[u32; COLS]; ROWS] = [[0; COLS]; ROWS];
 
     let (mut rl, thread) = init()
         .size(WIDTH, HEIGHT)
@@ -33,9 +39,9 @@ fn main() {
     rl.set_target_fps(fps);
 
     while !rl.window_should_close() {
-        controls(&mut running, &mut rl, ROWS, COLS, &mut cells, &mut fps);
-        update_sim(running, ROWS, COLS, &mut cells, &mut tmp_cells);
-        draw_game(&mut rl, &thread, &cells, ROWS, COLS, CELL_SIZE);
+        controls(&mut running, &mut rl, &mut cells, &mut fps);
+        update_sim(running, &mut cells, &mut tmp_cells);
+        draw_game(&mut rl, &thread, &cells, CELL_SIZE);
         set_tile(&mut rl, &thread, fps, running);
     }
 }
@@ -43,19 +49,17 @@ fn main() {
 fn controls(
     running: &mut bool,
     rl: &mut RaylibHandle,
-    rows: usize,
-    cols: usize,
-    cells: &mut Vec<Vec<u32>>,
+    cells: &mut [[u32; COLS]; ROWS],
     fps: &mut u32,
 ) {
     if rl.is_key_pressed(KeyboardKey::KEY_ENTER) {
         *running = !*running;
     }
     if rl.is_key_pressed(KeyboardKey::KEY_R) {
-        fill_random(rl, running, rows, cols, cells)
+        fill_random(rl, running, ROWS, COLS, cells)
     }
     if rl.is_key_pressed(KeyboardKey::KEY_C) {
-        clear_grid(running, rows, cols, cells);
+        clear_grid(running, ROWS, COLS, cells);
     }
     if rl.is_key_pressed(KeyboardKey::KEY_S) || rl.is_key_pressed(KeyboardKey::KEY_F) {
         if rl.is_key_pressed(KeyboardKey::KEY_F) {
@@ -71,14 +75,12 @@ fn controls(
 fn draw_game(
     rl: &mut RaylibHandle,
     thread: &RaylibThread,
-    cells: &Vec<Vec<u32>>,
-    rows: usize,
-    cols: usize,
+    cells: &[[u32; COLS]; ROWS],
     cell_size: usize,
 ) {
     let mut draw = rl.begin_drawing(thread);
     draw.clear_background(GREY);
-    draw_cells(&mut draw, cells, rows, cols, cell_size);
+    draw_cells(&mut draw, cells, ROWS, COLS, cell_size);
 }
 
 fn set_tile(rl: &mut RaylibHandle, thread: &RaylibThread, fps: u32, running: bool) {
@@ -94,7 +96,7 @@ fn set_tile(rl: &mut RaylibHandle, thread: &RaylibThread, fps: u32, running: boo
 
 fn draw_cells(
     draw: &mut RaylibDrawHandle,
-    cells: &Vec<Vec<u32>>,
+    cells: &[[u32; COLS]; ROWS],
     rows: usize,
     cols: usize,
     cell_size: usize,
@@ -103,7 +105,7 @@ fn draw_cells(
         for col in 0..cols {
             let color: Color;
             if cells[row][col] == 1 {
-                color = Color::GREEN;
+                color = GREEN;
             } else {
                 color = DARK_GREY;
             }
@@ -123,7 +125,7 @@ fn fill_random(
     running: &bool,
     rows: usize,
     cols: usize,
-    cells: &mut Vec<Vec<u32>>,
+    cells: &mut [[u32; COLS]; ROWS],
 ) {
     if !running {
         for row in 0..rows {
@@ -139,7 +141,7 @@ fn fill_random(
     }
 }
 
-fn clear_grid(running: &bool, rows: usize, cols: usize, cells: &mut Vec<Vec<u32>>) {
+fn clear_grid(running: &bool, rows: usize, cols: usize, cells: &mut [[u32; COLS]; ROWS]) {
     if !running {
         for row in 0..rows {
             for col in 0..cols {
@@ -149,13 +151,7 @@ fn clear_grid(running: &bool, rows: usize, cols: usize, cells: &mut Vec<Vec<u32>
     }
 }
 
-fn count_live_nbrs(
-    row: usize,
-    col: usize,
-    cells: &mut Vec<Vec<u32>>,
-    rows: usize,
-    cols: usize,
-) -> i32 {
+fn count_live_nbrs(row: usize, col: usize, cells: &[[u32; COLS]; ROWS]) -> i32 {
     let nbr_offsets: [[i32; 2]; 8] = [
         [-1, -1],
         [-1, 0],
@@ -170,8 +166,8 @@ fn count_live_nbrs(
     let mut live_nbrs = 0;
 
     for offset in nbr_offsets {
-        let new_row = (row as i32 + offset[0]).rem_euclid(rows as i32) as usize;
-        let new_col = (col as i32 + offset[1]).rem_euclid(cols as i32) as usize;
+        let new_row = (row as i32 + offset[0]).rem_euclid(ROWS as i32) as usize;
+        let new_col = (col as i32 + offset[1]).rem_euclid(COLS as i32) as usize;
         if cells[new_row][new_col] == 1 {
             live_nbrs += 1;
         }
@@ -180,17 +176,11 @@ fn count_live_nbrs(
     live_nbrs
 }
 
-fn update_sim(
-    running: bool,
-    rows: usize,
-    cols: usize,
-    cells: &mut Vec<Vec<u32>>,
-    tmp_cells: &mut Vec<Vec<u32>>,
-) {
+fn update_sim(running: bool, cells: &mut [[u32; COLS]; ROWS], tmp_cells: &mut [[u32; COLS]; ROWS]) {
     if running {
-        for row in 0..rows {
-            for col in 0..cols {
-                let live_nbrs = count_live_nbrs(row, col, cells, rows, cols);
+        for row in 0..ROWS {
+            for col in 0..COLS {
+                let live_nbrs = count_live_nbrs(row, col, cells);
                 let cell_value = cells[row][col];
 
                 if cell_value == 1 {
@@ -209,8 +199,8 @@ fn update_sim(
             }
         }
 
-        for row in 0..rows {
-            for col in 0..cols {
+        for row in 0..ROWS {
+            for col in 0..COLS {
                 cells[row][col] = tmp_cells[row][col];
             }
         }
