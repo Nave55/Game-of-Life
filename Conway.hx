488c8821a7f72ea6f4ae064863cd341ed548179d
Evan Martinez <147688099+Nave55@users.noreply.github.com>
2025-06-27 22:57:28 -0600

    Update Conway.hx

--- Conway.hx
+++ Conway.hx
@@ -1,14 +1,19 @@
 typedef AAI = Array<Array<Int>>;
 
 class Conway extends hxd.App {
-    var CELL_SIZE = 6;
-    var ROWS = 0;
-    var COLS = 0;
+    // constants 
+    static inline var CELL_SIZE = 6;
+    static inline var HEIGHT = 1000;
+    static inline var WIDTH = 1000;
+    static inline var GREEN = 0x00e430;
+    static inline var GREY = 0x373737;
+    static inline var DARK_GREY = 0x1d1d1d;
+    static inline var ROWS = Std.int(HEIGHT / CELL_SIZE);
+    static inline var COLS = Std.int(WIDTH / CELL_SIZE);
+
+    // globals
     var cells: AAI = [];
     var tmp_cells: AAI = [];
-    var GREEN = 0x00e430;
-    var GREY = 0x373737;
-    var DARK_GREY = 0x1d1d1d;
     var running = false;
     var gen = hxd.Rand.create();
     var graphics: h2d.Graphics;
@@ -19,7 +24,7 @@ class Conway extends hxd.App {
         new Conway();
     }
 
-    public function fillRandom() {
+    function fillRandom() {
         if (!running) {
             for (row in 0...ROWS) {
                 for (column in 0...COLS) {
@@ -30,7 +35,7 @@ class Conway extends hxd.App {
         }
     }
 
-    public function drawCells() {
+    function drawCells() {
         graphics.clear(); // Clear previous drawing
         for (row in 0...ROWS) {
             for (column in 0...COLS) {
@@ -42,7 +47,7 @@ class Conway extends hxd.App {
         }
     }
 
-    public function clearGrid() {
+    function clearGrid() {
         if (!running) {
             for (row in 0...ROWS) {
                 for (column in 0...COLS) {
@@ -53,7 +58,7 @@ class Conway extends hxd.App {
         }
     }
 
-    public function countLiveNbrs(row: Int, column: Int): Int {
+    function countLiveNbrs(row: Int, column: Int): Int {
         var live_neighbors = 0;
         var offsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
         for (offset in offsets) {
@@ -64,7 +69,7 @@ class Conway extends hxd.App {
         return live_neighbors;
     }
 
-    public function updateSim() {
+    function updateSim() {
         if (running) {
             for (row in 0...ROWS) {
                 for (column in 0...COLS) {
@@ -77,7 +82,7 @@ class Conway extends hxd.App {
                         if (live_neighbors == 3) 1 else 0;
                     };
                 }
-            }
+           }
             for (row in 0...ROWS) {
                 for (column in 0...COLS) {
                     if (cells[row][column] != tmp_cells[row][column]) {
@@ -92,10 +97,8 @@ class Conway extends hxd.App {
         }
     }
 
-    override public function init() {
+    override function init() {
         super.init();
-        ROWS = Std.int(s2d.height / CELL_SIZE);
-        COLS = Std.int(s2d.width / CELL_SIZE);
 
         var bg = new h2d.Graphics(s2d);
         bg.beginFill(GREY);
